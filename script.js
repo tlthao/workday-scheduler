4aedea2b5c741b1f94827c5c5fa247d4865bad1e
Tina Ly <t.tinaly@hotmail.com>
2021-07-13 01:26:36 -0700

    completed css and html...i think

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
  $(document).ready(function() {
     $('#current-day').text(moment().format(MM,DD,YYYY)); //moment.js date format//
-    $(".saveBtn").on("click", function () {
+    $(".save-btn").on("click", function () {
         var date = $(this).parent().attr("id");
         var text = $(this).siblings(".task").val();
         localStorage.setItem(date, text);
