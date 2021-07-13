2757736823f426cb10cc44e86335429a6b722449
Tina Ly <t.tinaly@hotmail.com>
2021-07-13 16:21:29 -0700

    adding jquery in js file

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
- $(document).ready(function() {
-    $('#current-day').text(moment().format(MM,DD,YYYY)); //moment.js date format//
-    $(".save-btn").on("click", function () {
-        var date = $(this).parent().attr("id");
-        var text = $(this).siblings(".task").val();
-        localStorage.setItem(date, text);
-    })
- });
+$(document).ready(function(){
+//saveBtn click
+    $(".saveBtn").on("click", function() {
+    var textInput = $(this).siblings(".task").val();
+    var  time = $(this).parent().attr("id");
+    }); 
+    $("#current-day").text(moment().format("dddd, MMMM Do"));
+    localStorage.setItem(time, textInput); //saving in the local storage
+  });
+//local storage for the hours
+
+$("#h-9 .task").val(localStorage.getItem("h-9"));
+$("#h-10 .task").val(localStorage.getItem("h-10"));
+$("#h-11 .task").val(localStorage.getItem("h-11"));
+$("#h-12 .task").val(localStorage.getItem("h-12"));
+$("#h-13 .task").val(localStorage.getItem("h-13"));
+$("#h-14 .task").val(localStorage.getItem("h-14"));
+$("#h-15 .task").val(localStorage.getItem("h-15"));
+$("#h-16 .task").val(localStorage.getItem("h-16"));
+$("#h-17 .task").val(localStorage.getItem("h-17"));
+
+
+  function hourUpdate();
+  var thisHour = moment().hours(); //retrieving the hours 
+
+  $(".time-block").each(function() {
+    var eachHour = parseInt($(this).attr("id").split("-")[1]);
+    if (eachHour < thisHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+    }
+    else if (eachHour == thisHour)
+        $(this).removeClass("past");
+        $(this).addClass("present");
+
+    }
