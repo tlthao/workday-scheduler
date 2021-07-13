bd905eab89495e2609bb2ba517e79a6daeadcbb2
Tina Ly <t.tinaly@hotmail.com>
2021-07-13 00:25:12 -0700

    editing schedule grid

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
  $(document).ready(function() {
     $('#current-day').text(moment().format(MM,DD,YYYY)); //moment.js date format//
-    
-
-
+    $(".saveBtn").on("click", function () {
+        var date = $(this).parent().attr("id");
+        var text = $(this).siblings(".task").val();
+        localStorage.setItem(date, text);
+    })
  });
-
-
-
-
