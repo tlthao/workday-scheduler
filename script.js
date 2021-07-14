ce4d0e4a5550502c71a31f44296454ae5458c7b0
Tina Ly <t.tinaly@hotmail.com>
2021-07-13 18:31:48 -0700

    editing the hours function, adjusting padding for textarea, tried adding a cursor but that didn't work

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
 $(document).ready(function(){
-//saveBtn click
-    $(".saveBtn").on("click", function() {
-    var textInput = $(this).siblings(".task").val();
-    var  time = $(this).parent().attr("id");
-    }); 
     $("#current-day").text(moment().format("dddd, MMMM Do"));
-    localStorage.setItem(time, textInput); //saving in the local storage
-  });
-//local storage for the hours
 
-$("#h-9 .task").val(localStorage.getItem("h-9"));
-$("#h-10 .task").val(localStorage.getItem("h-10"));
-$("#h-11 .task").val(localStorage.getItem("h-11"));
-$("#h-12 .task").val(localStorage.getItem("h-12"));
-$("#h-13 .task").val(localStorage.getItem("h-13"));
-$("#h-14 .task").val(localStorage.getItem("h-14"));
-$("#h-15 .task").val(localStorage.getItem("h-15"));
-$("#h-16 .task").val(localStorage.getItem("h-16"));
-$("#h-17 .task").val(localStorage.getItem("h-17"));
+    $(".saveBtn").on("click", function() { //saveBtn click
+        var textInput = $(this).siblings(".task").val();
+        var  time = $(this).parent().attr("id");
+        localStorage.setItem(time, textInput);
+    });
+
+        //saving text input in the local storage
+//local storage for each of the hours
+
+  $("#h-9 .task").val(localStorage.getItem("h-9"));
+  $("#h-10 .task").val(localStorage.getItem("h-10"));
+  $("#h-11 .task").val(localStorage.getItem("h-11"));
+  $("#h-12 .task").val(localStorage.getItem("h-12"));
+  $("#h-13 .task").val(localStorage.getItem("h-13"));
+  $("#h-14 .task").val(localStorage.getItem("h-14"));
+  $("#h-15 .task").val(localStorage.getItem("h-15"));
+  $("#h-16 .task").val(localStorage.getItem("h-16"));
+  $("#h-17 .task").val(localStorage.getItem("h-17"));
 
 
-  function hourUpdate();
-  var thisHour = moment().hours(); //retrieving the hours 
-
-  $(".time-block").each(function() {
-    var eachHour = parseInt($(this).attr("id").split("-")[1]);
-    if (eachHour < thisHour) {
-        $(this).removeClass("past");
-        $(this).addClass("present");
+    function hourChange() {
+        var thisHour = moment().hour(); //retrieving the hours 
+        console.log(thisHour)
+        $(".time-block").each(function() {
+            var otherHour = parseInt($(this).attr("id").split("-")[1]);
+            if (otherHour < thisHour) {
+                $(this).addClass("past");
+            }
+            else if (otherHour == thisHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+            
+        });
     }
-    else if (eachHour == thisHour)
-        $(this).removeClass("past");
-        $(this).addClass("present");
-
-    }
+    hourChange();
+});
